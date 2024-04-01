a213f32599b50652459fae99587af1e3b35fda2d
Дмитрий Поляков <electroni_k@mail.ru>
2024-04-02 01:26:32 +0300

    add index and class Status

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
     body = models.TextField()
@@ -9,12 +15,13 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
 
+
     class Meta:
         ordering = ['-publish']
         indexes = [
-            models.Index(fields='-publish')
+            models.Index(fields=['-publish'])
         ]
-
 
     def __str__(self):
         return self.title
+
