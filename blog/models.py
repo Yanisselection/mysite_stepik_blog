a3fb997f7fd5922ffbaa05dd05536033ff283470
Дмитрий Поляков <electroni_k@mail.ru>
2024-04-02 01:33:09 +0300

    update models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -7,14 +8,16 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
-
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+
     body = models.TextField()
+
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
-
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
@@ -24,4 +27,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
