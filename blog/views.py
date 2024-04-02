20c346378711d9563027890ad753ec113655f477
Дмитрий Поляков <electroni_k@mail.ru>
2024-04-02 19:13:59 +0300

    create templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
@@ -8,11 +7,5 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-
-    except Post.DoesNotExist:
-        raise Http404('No Post Found')
-
-    return render(request, 'blog/post/detail.html', {'post': post})
-        
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post })
