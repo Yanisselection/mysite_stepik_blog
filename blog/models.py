28e65c9a55da77d7221376ee5a534007cbeb5cd8
Дмитрий Поляков <electroni_k@mail.ru>
2024-04-04 13:06:55 +0300

    Видоизменение канонических УРЛ

--- blog/models.py
+++ blog/models.py
@@ -29,9 +29,12 @@ class Post(models.Model):
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
-
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
     class Meta:
         ordering = ['-publish']
