7053b5ebaed6fd8eb01a713e3a74e767e50bbb10
Дмитрий Поляков <electroni_k@mail.ru>
2024-04-04 19:07:10 +0300

    замена функций на классы в представлении

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,14 @@
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator
 
 
-# Create your views here.
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.get_page(page_number)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
