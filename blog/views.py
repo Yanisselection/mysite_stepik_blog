87c17fd89b9a56fff933214a6f7cbe3313e7ff78
Дмитрий Поляков <electroni_k@mail.ru>
2024-04-04 13:57:29 +0300

    Если страница указано неверно
        posts = paginator.get_page(page_number)

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    posts = paginator.get_page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
